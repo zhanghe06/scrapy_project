65c9c5e9b584075d88fb7f0e63338b101e0f3d03
Zhang He <zhang_he06@163.com>
2017-03-26 00:00:00 +0800

    更新配置文件

--- init.env.sh
+++ init.env.sh
@@ -3,10 +3,3 @@ source scrapy.env/bin/activate
 export PYTHONPATH=`pwd`/app
 
 CONFIG_MODE_TEXT=`cat $PYTHONPATH/app/config/config_mode`
-
-# alias supervisorctl="supervisorctl -c $PYTHONPATH/app/config/$CONFIG_MODE_TEXT/supervisord.conf"
-
-# alias supervilord="supervisord -c $PYTHONPATH/app/config/$CONFIG_MODE_TEXT/supervisord.conf"
-
-echo "sudo ln -s -f $PYTHONPATH/app/config/$CONFIG_MODE_TEXT/supervisord.conf etc/supervisord.conf"
-sudo ln -s -f $PYTHONPATH/app/config/$CONFIG_MODE_TEXT/supervisord.conf etc/supervisord.conf
